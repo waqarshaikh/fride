79004bfc401b3ad5698ea65700c6a242a71afbad
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-07-08 12:09:41 +0530

    Add Stream Provider and Loading Spinner

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fride/models/custom_user.dart';
 import 'package:fride/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fride/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +15,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<CustomUser?>.value(
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fride/models/custom_user.dart';
 import 'package:fride/screens/authenticate/authenticate.dart';
 import 'package:fride/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    final user = Provider.of<CustomUser?>(context);
+    return user != null ? Home() : Authenticate();
   }
 }
