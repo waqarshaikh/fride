e23e020add0a3c7ff526f25fc2dc6378ad09596f
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-07-08 12:06:47 +0530

    Add Email&Password signin and register, Stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,12 +1,60 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fride/models/custom_user.dart';
 
 class AuthService {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //create CustomUser object based on User object from Firebase
+  CustomUser? _userFromFirebaseUser(User? user) {
+    return user != null ? CustomUser(uid: user.uid) : null;
+  }
+
+  //Auth change User Stream
+  Stream<CustomUser?> get user {
+    return _auth
+        .authStateChanges()
+        .map((User? user) => _userFromFirebaseUser(user));
+  }
+
+  //Sign in Anonymously
   Future signInAnonymous() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
-      return result.user;
+      return _userFromFirebaseUser(result.user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Register with Email and Password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return _userFromFirebaseUser(result.user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Register with Email and Password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return _userFromFirebaseUser(result.user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
     } catch (e) {
       print(e.toString());
       return null;
