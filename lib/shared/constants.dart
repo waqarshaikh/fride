313e33ed47dd3b84942fa910af17f7aff84556d9
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-07-08 12:08:53 +0530

    Add inputDecoration constant

---
+++ lib/shared/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const inputDecoration = InputDecoration(
+  fillColor: Colors.white,
+  filled: true,
+  focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+    color: Colors.blue,
+    width: 2.0,
+  )),
+  enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+    color: Colors.white,
+    width: 2.0,
+  )),
+);
