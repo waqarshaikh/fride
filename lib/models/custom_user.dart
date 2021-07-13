f3a62bf858b3e0158b819705444a056528459ded
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-07-14 01:27:30 +0530

    add UserLocation model

--- lib/models/custom_user.dart
+++ lib/models/custom_user.dart
@@ -1,6 +1,14 @@
 class CustomUser {
   final String? uid;
   final String? name;
+  final String? role;
 
-  CustomUser({ this.uid, this.name});
+  CustomUser({this.uid, this.name, this.role});
+}
+
+class UserLocation {
+  final double? latitude;
+  final double? longitude;
+
+  UserLocation({this.latitude, this.longitude});
 }
