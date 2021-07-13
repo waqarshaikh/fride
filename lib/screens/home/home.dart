831179c3d838f39ef855a3ee80ccdc1732cd056b
Waqar Shaikh <shaikhwaqar1999@gmail.com>
2021-07-14 01:33:05 +0530

    Remove trivial code and minor changes

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:fride/models/custom_user.dart';
+import 'package:fride/screens/home/custom_map.dart';
 import 'package:fride/services/auth.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:fride/services/location_service.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -14,64 +14,31 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
-  Position? currentPosition;
-  var geoLocator = Geolocator();
-
-  void locatePosition() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    currentPosition = position;
-
-    LatLng latlan = LatLng(position.latitude, position.longitude);
-
-    CameraPosition cameraPosition = CameraPosition(target: latlan, zoom: 16);
-
-    newController!
-        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-  }
-
-  Completer<GoogleMapController> _controller = Completer();
-  GoogleMapController? newController;
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Home")),
-        backgroundColor: Colors.blue[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          TextButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.logout, color: Colors.white),
-              label: Text(
-                "Log Out",
-                style: TextStyle(color: Colors.white),
-              ))
-        ],
-      ),
-      body: Stack(
-        children: [
-          GoogleMap(
-            mapType: MapType.normal,
-            myLocationButtonEnabled: true,
-            initialCameraPosition: _kGooglePlex,
-            myLocationEnabled: true,
-            zoomControlsEnabled: true,
-            zoomGesturesEnabled: true,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-              newController = controller;
 
-              locatePosition();
-            },
-          ),
-        ],
+    return StreamProvider<UserLocation>.value(
+      initialData: UserLocation(latitude: 70.0, longitude: 20.0),
+      value: LocationService().locationStream,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Home")),
+          backgroundColor: Colors.blue[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            TextButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.logout, color: Colors.white),
+                label: Text(
+                  "Log Out",
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+        body: CustomMap() 
       ),
     );
   }
